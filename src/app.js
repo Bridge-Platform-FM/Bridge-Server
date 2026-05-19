1ec1b7d1e29b9bfec069cd256483534a54be1690
abhinavFM <abhinav.vaidya@flairminds.com>
2026-05-19 18:34:07 +0530

    Added EsLint

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const config = require('./configs/configs');
 
 const app = express();
 app.use(express.json());
@@ -6,9 +7,10 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+const SERVER_PORT = config.SERVER_PORT || 3000;
 
-app.listen(3000, () => {
-    console.log('Server is running on port http://localhost:3000');
+app.listen(SERVER_PORT, () => {
+    console.info(`Server is running on port http://localhost:${SERVER_PORT}`);
 });
 
 module.exports = app;
---
+++ src/configs/configs.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+const env = {
+    SERVER_PORT: process.env.SERVER_PORT
+};
+
+module.exports = env;

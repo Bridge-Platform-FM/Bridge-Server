83b698b785720a9d1d10fe51f15edfc915573cd5
dev-gaurav <gaurav.singh@flairminds.com>
2026-06-16 18:27:58 +0530

    uat setup added

--- src/configs/redis.js
+++ src/configs/redis.js
@@ -5,11 +5,13 @@ const redis = new Redis({
 //   host: "https://c6de-103-191-205-18.ngrok-free.app",
 //   port: 6381,
 
-  host: process.env.REDIS_HOST || 'localhost',
-  port: process.env.REDIS_PORT || 6381,
-
-  password: process.env.REDIS_PASSWORD,
+    host: process.env.REDIS_HOST || 'localhost',
+    port: process.env.REDIS_PORT || 6381,
+    password: process.env.REDIS_PASSWORD,
+    // uat
+    // username: process.env.REDIS_USERNAME,
 });
+
 
 redis.on('connect', () => {
     console.info("Redis Connection Established");

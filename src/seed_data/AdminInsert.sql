047e215ff9062d89321eaa482a347ebdec97bad2
abhinavFM <abhinav.vaidya@flairminds.com>
2026-07-30 20:20:22 +0530

    updated admin ids in otp-config to uuid

--- src/seed_data/AdminInsert.sql
+++ src/seed_data/AdminInsert.sql
@@ -1,4 +1,11 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
 INSERT INTO public."admin"
-("name", email, "password", "role", country_code, mobile_number, created_at)
-VALUES('admin', 'admin@test.com', '$2b$10$wmcoo0vW/EJcFjq37y9WQ.Du0elbaIQnEq.YOaeX72MToGMQYE0Gu', 'ADMIN', '+91', '8612345678', now());
+(id, "name", email, "password", "role", country_code, mobile_number, created_at)
+VALUES(gen_random_uuid(), 'admin', 'admin@test.com', '$2b$10$wmcoo0vW/EJcFjq37y9WQ.Du0elbaIQnEq.YOaeX72MToGMQYE0Gu', 'ADMIN', '+91', '8612345678', now());
+---- -- Admin@123456
+
+INSERT INTO public."admin"
+(id, "name", email, "password", "role", country_code, mobile_number, created_at)
+VALUES(gen_random_uuid(), 'super_admin', 'super_admin@test.com', '$2b$10$wmcoo0vW/EJcFjq37y9WQ.Du0elbaIQnEq.YOaeX72MToGMQYE0Gu', 'SUPER_ADMIN', '+91', '8612345679', now());
 ---- -- Admin@123456

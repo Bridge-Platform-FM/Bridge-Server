1b0274a05be9314757f2fa70d4d565e3fbe2759c
dev-gaurav <gaurav.singh@flairminds.com>
2026-08-11 15:30:16 +0530

    env fixes

--- config/config.js
+++ config/config.js
@@ -8,12 +8,11 @@ module.exports = {
         host: process.env.DB_HOST, 
         dialect: process.env.DB_DILECT, 
         port: process.env.DB_PORT,
-        // UAT
-        // dialectOptions: {
-        //     ssl: {
-        //         require: true,
-        //         rejectUnauthorized: false
-        //     }
-        // }
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
     }
 };
